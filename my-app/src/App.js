f69c911bfff1697be5e459027329c392370f536a
Leena <leenamahajan1707@gmail.com>
2021-09-02 15:02:52 +0530

    V16

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,16 @@
 
 import './App.css';
-// import About from './components/About';
+import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react'
 import Alert from './components/Alert';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 
 function App() {
@@ -39,12 +45,21 @@ function App() {
 
   return (
     <>
-     <Navbar title="TextUtils" aboutText="About US" mode={mode} toggleMode = {toggleMode}/>
+    <Router>
+
+     <Navbar title="TextUtils" aboutText="About" mode={mode} toggleMode = {toggleMode}/>
      <Alert alert ={alert}/>
      <div className="container my-3">
-      <TextForm showAlert = {showAlert} heading="Enter the text to analyze below"  mode={mode}/>
-      {/* <About/> */}
+     <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+           <Route exact path="/">    {/*react will do exact matching with exact keyword otherwise it will do partial matching*/}
+            <TextForm showAlert = {showAlert} heading="Enter the text to analyze below"  mode={mode}/>
+          </Route>
+      </Switch>
     </div>
+    </Router>
     </>
     
   );
