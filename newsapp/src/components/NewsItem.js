eabef6607b71563622e0e03b853a92056d1a8e8d
Leena <leenamahajan1707@gmail.com>
2021-09-03 17:34:49 +0530

    V27 fetch API

--- newsapp/src/components/NewsItem.js
+++ newsapp/src/components/NewsItem.js
@@ -7,7 +7,7 @@ export class NewsItem extends Component {
         return (
             <div className="my-3">
                 <div className="card" style={{width: "18rem"}}>
-                    <img src={imgUrl} className="card-img-top" alt="..."/>
+                    <img src={imgUrl ? imgUrl : "https://images.indianexpress.com/2021/09/nasa.jpg" } className="card-img-top" alt="..."/>
                     <div className="card-body">
                         <h5 className="card-title">{title}...</h5>
                         <p className="card-text">{description}...</p>
