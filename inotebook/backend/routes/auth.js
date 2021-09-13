ecc5361ddc869556dbe97deddedcb8e675b55f51
Leena <leenamahajan1707@gmail.com>
2021-09-13 12:48:24 +0530

    V45 storing data into DB using mongoose

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const User = require('../models/User');
 const router = express.Router();
 
-router.get('/', (req, res) => {
 
-    res.json([])
+//create a User using: POST "/api/auth/"    Doesn't require Auth
+router.post('/', (req, res) => {
+    console.log(req.body);
+    const user = User(req.body);
+    user.save();
+    res.send(req.body);
 })
 
 
