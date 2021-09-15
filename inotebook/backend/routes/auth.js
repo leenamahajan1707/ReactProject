0cd7149486e8f7327a357fb7b8fd4e5f6ef6c82a
Leena <leenamahajan1707@gmail.com>
2021-09-15 12:56:27 +0530

    V49 hashing password using bcryptjs

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+var bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
+
+const JWT_SECRET = "Leenalearning$react"
 
 
 //create a User using: POST "/api/auth/createuser"    Doesn't require Login
@@ -24,13 +28,27 @@ router.post('/createuser', [
         if (user) {
             return res.status(400).json({ error: "sorry a user with this email already exists " })
         }
+
+        //bcrypt is used to encrypt the password
+        const salt = await bcrypt.genSalt(10);
+        const secPass = await bcrypt.hash(req.body.password, salt)
+
+        //create a new user
         user = await User.create({
             name: req.body.name,
-            email: req.body.email,
-            password: req.body.password
+            password: secPass,
+            email: req.body.email
         })
 
-        res.json(user)
+        const data = {
+            user: {
+                id: user.id
+            }
+        }
+        //jwttoken helps to maintain security
+        const authtoken = jwt.sign(data, JWT_SECRET)
+
+        res.json({ authtoken })
 
     } catch (error) {
         console.error(error.message);
