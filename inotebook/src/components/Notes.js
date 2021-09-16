b52d44fe8d317fd6d7cb07680f16a6aa93f05727
Leena <leenamahajan1707@gmail.com>
2021-09-16 19:49:44 +0530

    V61 Adding NoteItem component which shows Notes

--- inotebook/src/components/Notes.js
+++ inotebook/src/components/Notes.js
@@ -1,14 +1,16 @@
 import React, { useContext } from 'react'
 import noteContext from '../context/Notes/noteContext';
+import NoteItem from './NoteItem';
 
 const Notes = () => {
+    //using notecontext
     const context = useContext(noteContext);
     const { notes, setNotes } = context;
     return (
-        <div className="container my-3">
+        <div className="row my-3">
             <h2>Your Notes</h2>
             {notes.map((note) => {
-                return note.title
+                return <NoteItem note={note} />
             })}
         </div>
     )
