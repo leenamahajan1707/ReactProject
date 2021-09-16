a62ea28f5be503ecd4e22743ffb22a10982bddf7
Leena <leenamahajan1707@gmail.com>
2021-09-16 18:21:15 +0530

    V59 useLocation hook

--- inotebook/src/components/About.js
+++ inotebook/src/components/About.js
@@ -1,16 +1,10 @@
-import React, { useContext, useEffect } from 'react'
-import noteContext from '../context/Notes/noteContext'
-
+import React from 'react'
 
 const About = () => {
-    const a = useContext(noteContext)
-    useEffect(() => {
-        a.update()
-        // eslint-disable-next-line
-    }, [])
+
     return (
         <div>
-            About {a.state.name}
+            About
         </div>
     )
 }
