d95ca56a9abe64e6d76ed6af1fc946cc8fb70911
Leena <leenamahajan1707@gmail.com>
2021-09-16 12:37:05 +0530

    V53 updating existing note

--- inotebook/backend/routes/notes.js
+++ inotebook/backend/routes/notes.js
@@ -40,4 +40,35 @@ router.post('/addnote', fetchuser, [
     }
 })
 
+//ROUTE 3 : update an existing  Notes using: POST "/api/notes/updatenote"    require Login
+router.put('/updatenote/:id', fetchuser, async (req, res) => {
+    try {
+        const { title, description, tag } = req.body;
+
+        //create a newNote object
+        const newNote = {};
+        if (title) { newNote.title = title };
+        if (description) { newNote.description = description };
+        if (tag) { newNote.tag = tag };
+
+        //find the note to be updated and update it
+        let note = await Note.findById(req.params.id);
+        if (!note) {
+            return res.status(404).send("Not found")
+        }
+        if (note.user.toString() !== req.user.id) {
+            return res.status(401).send("not allowed")
+        }
+
+        note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true })
+        res.json({ note });
+
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Internal server Error");
+    }
+
+})
+
+
 module.exports = router
