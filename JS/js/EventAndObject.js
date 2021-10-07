f955e4b2b280c17a56c9db2bb25004ad6fc33709
Leena <leenamahajan1707@gmail.com>
2021-10-07 12:28:43 +0530

    v18 - events

--- JS/js/EventAndObject.js
+++ JS/js/EventAndObject.js
@@ -1,12 +1,44 @@
-console.log('This is tut 17');
+console.log('This is tut 17,18');
 
-document.getElementById('heading').addEventListener('click', function (e) {
-    console.log('clicked heading');
-    variable = e.target;
-    variable = e.target.class;
-    variable = Array.from(e.target.classList);
-    variable = e.offsetX; //how much long you clicked on ele
-    variable = e.offsetY;
-    console.log(variable);
-    // location.href = "//codewithharry.com"
+// document.getElementById('heading').addEventListener('click', function (e) {
+//     console.log('clicked heading');
+//     variable = e.target;
+//     variable = e.target.class;
+//     variable = Array.from(e.target.classList);
+//     variable = e.offsetX; //how much long you clicked on ele
+//     variable = e.offsetY;
+//     console.log(variable);
+//     // location.href = "//codewithharry.com"
+// })
+
+// let btn = document.getElementById('btn');
+// btn.addEventListener('click', func1);
+// btn.addEventListener('dblclick', func2);
+// btn.addEventListener('mousedown', func3);
+
+// function func1(e) {
+//     console.log("thanks", e);
+//     e.preventDefault();
+// }
+// function func2(e) {
+//     console.log("thanks dbl click", e);
+//     e.preventDefault();
+// }
+// function func3(e) {
+//     console.log("thanks mousedown", e);
+//     e.preventDefault();
+// }
+
+// document.querySelector('.no').addEventListener('mouseenter', function () {
+//     console.log('you entered no');
+
+// })
+// document.querySelector('.no').addEventListener('mouseleave', function () {
+//     console.log('you leaved no');
+
+// })
+document.querySelector('.container').addEventListener('mousemove', function (e) {
+    console.log('mouse move event');
+    document.body.style.backgroundColor = `rgb(${e.offsetX},${e.offsetX},154`
+
 })
